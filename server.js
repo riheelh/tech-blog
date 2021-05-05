6bd47a25e7d8bcb78dc7ba21dfac5bbeb7f2114a
Riheel Hamoande <riheel83@gmail.com>
2021-05-05 10:12:56 -0500

    working User, Post Model & seed

--- server.js
+++ server.js
@@ -10,3 +10,33 @@ const routes = require('./controllers');
 const helpers = require('./utils/helpers');
 const sequelize = require('./config/connection');
 
+// ----------- express -----------
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+const hbs = exphbs.create({ helpers });
+
+// ----------- session -----------
+const sess = {
+    secret: 'Super secret secret',
+    cookie: {},
+    resave: false,
+    saveUninitialized: true,
+    store: new SequelizeStore({
+      db: sequelize
+    })
+};
+
+// ----------- middleware -----------
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(routes);
+
+// ----------- establish connection -----------
+sequelize.sync({ force: false }).then(() => {
+app.listen(PORT, () => console.log('Now listening'));
+});
