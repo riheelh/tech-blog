875ef3b2158370b167b7b5711f194d7d9d51d04f
Riheel Hamoande <riheel83@gmail.com>
2021-05-13 12:07:02 -0500

    Add create/delete post functions

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,10 +1,10 @@
 const router = require('express').Router();
 const userRoutes = require('./userRouters');
-// const postRoutes = require('./postRoutes');
+const postRoutes = require('./postRoutes');
 // const commentRoutes = require('./commentRoutes');
 
 router.use('/users', userRoutes);
-// router.use('/posts', postRoutes);
+router.use('/posts', postRoutes);
 // router.use('/comments', commentRoutes);
 
 module.exports = router;
---
+++ public/js/dashboard.js
@@ -0,0 +1,68 @@
+const newFormHandler = async (event) => {
+    event.preventDefault();
+
+    const title = document.querySelector('#post-name').value.trim();
+    const content = document.querySelector('#post-desc').value.trim();
+
+    if (title && content) {
+        const response = await fetch(`/api/posts`, {
+        method: 'POST',
+        body: JSON.stringify({ title, content }),
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        });
+
+        if (response.ok) {
+        document.location.replace('/dashboard');
+        } else {
+        alert('Failed to create post');
+        }
+    }
+};
+  
+const delButtonHandler = async (event) => {
+    if (event.target.hasAttribute('data-id')) {
+      const id = event.target.getAttribute('data-id');
+  
+      const response = await fetch(`/api/posts/${id}`, {
+        method: 'DELETE',
+      });
+  
+      if (response.ok) {
+        document.location.replace('/dashboard');
+      } else {
+        alert('Failed to delete post');
+      }
+    }
+};
+
+const editButtonHandler = async (event) => {
+    if (event.target.hasAttribute('data-id')) {
+      const id = event.target.getAttribute('data-id');
+  
+      const response = await fetch(`/api/posts/${id}`, {
+        method: 'PUT',
+      });
+  
+      if (response.ok) {
+        document.location.replace('/dashboard');
+      } else {
+        alert('Failed to delete post');
+      }
+    }
+};
+
+
+
+document.querySelector('.new-post-form')
+    .addEventListener('submit', newFormHandler);
+
+
+
+document.querySelectorAll('.delete-post')
+    .forEach((element) => {
+    element.addEventListener('click', delButtonHandler);
+});
+
+
